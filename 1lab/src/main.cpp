15b89b406cd60578cecd3087e7618c4431fac004
Volkov Alexey <lexav2016@mail.ru>
2023-09-10 21:08:50 +0300

    Some unimportant changes

--- 1lab/src/main.cpp
+++ 1lab/src/main.cpp
@@ -10,12 +10,8 @@ bool is_vowel(char c) {
 
 using namespace std;
 int main() {
-    string s;
-    cin >> s;
-    for (int i = 0; i < s.length(); i++) {
-        if (is_vowel(s[i])) {
-            s.erase(i, 1);
-        }
-    }
+    string s; cin >> s;
+    for (int i = 0; i < s.length(); i++)
+        if (is_vowel(s[i])) {s.erase(i, 1);}
     cout << s << endl;
 } 
