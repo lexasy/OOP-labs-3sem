593271105d3684c1c586a8451be33300db1545d0
Volkov Alexey <lexav2016@mail.ru>
2023-09-16 14:29:30 +0300

    Some upgrades

--- 1lab/src/task.cpp
+++ 1lab/src/task.cpp
@@ -1,7 +1,8 @@
+#include <cctype>
 #include "task.h"
 
 static bool is_vowel(char c) {
-    if (c == 'a' || c =='e' || c == 'i' || c == 'o' || c =='u'  || c == 'y' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U' || c == 'Y') {
+    if (c == 'a' || c =='e' || c == 'i' || c == 'o' || c =='u'  || c == 'y') {
         return true;
     }
     return false;
@@ -11,7 +12,7 @@ using namespace std;
 string task06(string s) {
     int cnt = s.length();
     for (int i = 0; i < cnt; i++)
-        if (is_vowel(s[i])) {
+        if (is_vowel(tolower(s[i]))) {
             s.erase(i, 1); --i; // Декриментируем индекс, так как после стирания литеры, индекс следующей литеры будет уменьшен, и так мы ее не пропустим
         }
     return s;
