ec96ab91fa177e0cba03f5a085f37754fc4be21d
Volkov Alexey <lexav2016@mail.ru>
2023-11-05 19:48:22 +0300

    lab4: figure_array is done, but didn't test

--- 4lab/src/includes/figure_array.hpp
+++ 4lab/src/includes/figure_array.hpp
@@ -7,12 +7,37 @@
 template <Numeric T>
 class Figure_array {
 public:
-    Figure_array();
-    void append_figure(std::shared_ptr<Figure<T>> fig);
-    void rmv_figure(size_t idx);
-    std::shared_ptr<Figure<T>[]> array;
+    Figure_array() {
+        this->array = std::shared_ptr<std::shared_ptr<Figure<T>>[]>(new std::shared_ptr<Figure<T>>[CAPACITY]);
+        this->size = 0; this->capacity = CAPACITY;
+    }
+    void append_figure(std::shared_ptr<Figure<T>> fig) {
+        if (this->size == this->capacity) {
+            this->expand();
+        }
+        this->array[this->size] = fig;
+        this->size++;
+    }
+    void rmv_figure(size_t idx) {
+        this->array[idx] = nullptr;
+        for (size_t i = idx; i < this->size - 1; i++) {
+            this->array[i] = this->array[i + 1];
+        }
+        this->size--;
+    }
+    std::shared_ptr<std::shared_ptr<Figure<T>>[]> array;
     size_t size;
+    ~Figure_array() {
+        this->size = 0; this->capacity = 0;
+    }
+private:
     size_t capacity;
-    ~Figure_array();
+    void expand() {
+        std::shared_ptr<std::shared_ptr<Figure<T>>[]> new_array = std::shared_ptr<std::shared_ptr<Figure<T>>[]>(new std::shared_ptr<Figure<T>>[this->capacity + CAPACITY]);
+        for (size_t i = 0; i < this->size; i++) {
+            new_array[i] = this->array[i];
+        }
+        this->array = new_array; this->capacity += CAPACITY;
+    }
 };
 
--- 4lab/src/main.cpp
+++ 4lab/src/main.cpp
@@ -1,6 +1,4 @@
-#include "includes/pentagon.hpp"
-#include "includes/hexagon.hpp"
-#include "includes/octagon.hpp"
+#include "includes/figure_array.hpp"
 
 int main() {
     Point<int> pt1(1, 1); Point<int> pt2(1, 1); Point<int> pt3(1, 1); Point<int> pt4(1, 1); Point<int> pt5(1, 1);
