03e8f7ffa8ec0ce2184a1cbb532a13350e855997
Volkov Alexey <lexav2016@mail.ru>
2023-11-26 22:29:22 +0300

    lab05: const iterator, erase, insert, expand, shrink methods were minimal tested

--- 5lab/src/dynamic_array.hpp
+++ 5lab/src/dynamic_array.hpp
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 #include <memory>
+#include <iostream>
 
 #define DEFAULT 1
 
@@ -9,26 +10,25 @@ namespace my_nsp {
     template <class T, class Allocator>
     class Dynamic_array {
     private:
-        class Array_item {
-            T value;
-            bool free;
-        public:
-            Array_item() : value(0), free(false) {};
-            friend Dynamic_array;
-        };
-        using ALLOCATOR = typename Allocator::template rebind<Array_item>::other;
-        ALLOCATOR allocator;
-        Array_item *_buffer;
+        Allocator allocator;
+        T *_buffer;
         std::size_t _size;
         std::size_t _capacity;
         void expand() {
-            Array_item *new_buffer = allocator.allocate(_capacity);
+            T *new_buffer = allocator.allocate(_capacity);
             for (std::size_t i = 0; i < _size; i++) {
                 new_buffer[i] = _buffer[i];
-                _buffer[i].value = 0;
             }
             _buffer = new_buffer;
             _capacity *= 2;
+        }
+        void shrink() {
+            for (std::size_t i = 0; i < _size; i++) {
+                _buffer[i + _size] = _buffer[i];
+                allocator.deallocate(&_buffer[i], DEFAULT);
+            }
+            _buffer += _size;
+            _capacity /= 2;
         }
     public:
         Dynamic_array() {
@@ -40,16 +40,18 @@ namespace my_nsp {
         private:
             std::size_t _size;
             std::size_t _idx;
-            Array_item *_array;
+            T *_array;
         public:
+            friend Dynamic_array;
             using iterator_category = std::forward_iterator_tag;
             using difference_type = std::ptrdiff_t;
             using value_type = T;
             using pointer = T *;
+            using const_pointer = const T *;
             using reference = T&;
             using const_reference = const T&;
             Iterator() : _array(nullptr), _idx(0), _size(0) {};
-            Iterator(Array_item *arr, std::size_t i, std::size_t sz) : _array(arr), _idx(i), _size(sz) {};
+            Iterator(T *arr, std::size_t i, std::size_t sz) : _array(arr), _idx(i), _size(sz) {};
             Iterator(const Iterator& other) : _array(other._array), _idx(other._idx), _size(other._size) {};
             Iterator& operator++() {
                 if (_size <= _idx) {
@@ -65,15 +67,51 @@ namespace my_nsp {
                 return (other._idx != this->_idx) || (other._array != this->_array);
             }
             T& operator*() {
-                return this->_array[this->_idx].value;
+                return this->_array[this->_idx];
             }
             T& operator->() {
-                return this->_array[this->_idx].value;
+                return this->_array[this->_idx];
+            }
+        };
+        class Const_Iterator {
+        private:
+            std::size_t _size;
+            std::size_t _idx;
+            T const *_array;
+        public:
+            friend Dynamic_array;
+            using iterator_category = std::forward_iterator_tag;
+            using difference_type = std::ptrdiff_t;
+            using value_type = T;
+            using const_pointer = const T *;
+            using const_reference = const T&;
+            Const_Iterator() : _array(nullptr), _idx(0), _size(0) {};
+            Const_Iterator(T const *arr, std::size_t i, std::size_t sz) : _array(arr), _idx(i), _size(sz) {};
+            Const_Iterator(const Const_Iterator& other) : _array(other._array), _idx(other._idx), _size(other._size) {};
+            Const_Iterator& operator++() {
+                if (_size <= _idx) {
+                    throw std::logic_error("Out of bound"); 
+                }
+                this->_idx++;
+                return *this;
+            }
+            bool operator==(const Const_Iterator& other) {
+                return (other._array == this->_array) && (other._idx == this->_idx);
+            }
+            bool operator!=(const Const_Iterator& other) {
+                return (other._idx != this->_idx) || (other._array != this->_array);
+            }
+            T& operator*() {
+                return this->_array[this->_idx];
+            }
+            T& operator->() {
+                return this->_array[this->_idx];
             }
         };
         using const_pointer = const T *;
         using const_reference = const T&;
         using iterator = Iterator;
+        using const_iterator = Const_Iterator;
         using pointer = T *;
         using reference = T&;
         using size_type = std::size_t;
@@ -84,16 +122,49 @@ namespace my_nsp {
         iterator end() {
             return Iterator(this->_buffer, this->_size, this->_size);
         }
+        const_iterator cbegin() {
+            return Const_Iterator(this->_buffer, 0, this->_size);
+        }
+        const_iterator cend() {
+            return Const_Iterator(this->_buffer, this->_size, this->_size);
+        }
+        iterator insert(iterator position, T value) {
+            if (_size == _capacity) {
+                expand();
+            }
+            _buffer = allocator.allocate(DEFAULT);
+            for (std::size_t i = 0; i <= position._idx; i++) {
+                _buffer[i] = _buffer[i + 1];
+            }
+            _buffer[position._idx] = value;
+            position._size++;
+            _size++;
+            return position;
+        }
+        iterator erase(iterator position) {
+            for (std::size_t i = position._idx; i > 0; i--) {
+                _buffer[i] = _buffer[i - 1];
+            }
+            allocator.deallocate(&_buffer[0], DEFAULT);
+            position._size--;
+            _buffer++;
+            _size--;
+            return position;
+        }
+        // void shrink_to_fit() {
+
+        // }
         void pop_back() {
-            allocator.deallocate(&(this->_buffer[this->_size]), DEFAULT);
-            this->_buffer[this->_size].free = true;
             this->_size--;
+            if (this->_size <= this->_capacity / 2) {
+                shrink();
+            }
         }
         void push_back(T value) {
             if (_size == _capacity) {
                 expand();
             }
-            this->_buffer[this->_size++].value = value;
+            this->_buffer[this->_size++] = value;
         }
         std::size_t size() {
             return this->_size;
@@ -105,7 +176,10 @@ namespace my_nsp {
             if (idx >= _size) {
                 throw std::exception();
             }
-            return this->_buffer[idx].value;
+            return this->_buffer[idx];
+        }
+        void operator=(const Dynamic_array<T, Allocator>& other) {
+            return;
         }
     };
 }
--- 5lab/src/main.cpp
+++ 5lab/src/main.cpp
@@ -4,9 +4,14 @@
 
 int main() {
     my_nsp::Dynamic_array<int, my_nsp::Allocator<int, 16>> arr;
-    for (std::size_t i = 0; i < 16; i++) {
+    for (std::size_t i = 0; i < 4; i++) {
         arr.push_back(i);
+        std::cout << arr.capacity() << "\n";
     }
     auto it = arr.end();
-    std::cout << *it << "\n";
+    it = arr.erase(it);
+    for (std::size_t i = 0; i < arr.size(); i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
 }
